6ec8e7b7b69f377a58c32e2a1f43ad3c11b369ea
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2025-02-27 14:44:57 +0700

    Jobsheet

--- PWL_Jobsheet2_2025/app/Http/Controllers/WelcomeController.php
+++ PWL_Jobsheet2_2025/app/Http/Controllers/WelcomeController.php
@@ -10,6 +10,8 @@ class WelcomeController extends Controller
         return 'Hello World';
        }
     public function greeting(){
-        return view('blog.hello', ['name' => 'Andi']);
-        }
+        return view('blog.hello')
+        ->with('name','Andi')
+        ->with('occupation','Astronaut');
+    }   
 }
